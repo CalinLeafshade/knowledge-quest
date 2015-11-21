05f921aaf592ccc108b7b310438d64433b37d401
Steven Poulton <poulton.steven@googlemail.com>
2015-11-21 23:39:12 +0000

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        uglify: {
+            my_target: {
+                options: {
+                    sourceMap: true,
+                },
+                files: { 'public/app.js': ['src/js/app.js', 'src/**/*.js'] },
+            }
+        },
+        less: {
+            development: {
+                options: {
+                    paths: ["src/less"]
+                },
+                files: {
+                    "public/style.css": "src/less/style.less"
+                }
+            },
+            production: {
+                options: {
+                    paths: ["src/less"],
+                    plugins: [
+                        new (require('less-plugin-autoprefix'))({browsers: ["last 2 versions"]})
+                    ],
+                    modifyVars: {
+                        //imgPath: '"http://mycdn.com/path/to/images"',
+                        //bgColor: 'red'
+                    }
+                },
+                files: {
+                    "public/style.css": "src/less/style.less"
+                }
+            }
+        },
+        bower: {
+            install: {
+                options: {
+                    targetDir: './public/lib',
+                    layout: 'byType',
+                    install: true,
+                    verbose: false,
+                    cleanTargetDir: false,
+                    cleanBowerDir: false,
+                    bowerOptions: {}
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['<%= jshint.files %>'],
+                tasks: ['jshint', 'uglify']
+            },
+            styleSheets: {
+                files: ['src/less/*.less'],
+                tasks: ['less']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-bower-task');
+
+    grunt.registerTask('default', ['bower', 'less', 'uglify']);
+
+};
